e3b36227502f5033d90b81e0c1151d703108d621
abelrobel7@gmail.com <abelrobel7@gmail.com>
2022-08-21 11:49:55 +0300

    add functionality

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const selection = document.querySelectorAll(".btn");
-
-selection.addEventListener("click", () => {
-  console.log("clicked");
-});
-
-selection.forEach((select) => {
-  console.log(select.textContent);
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit");
+const rate = document.getElementById("rate-field");
+const submit = document.getElementById("submit-field");
+for (let i = 0; i < selection.length; i++) {
+  const element = selection[i];
+  element.addEventListener("click", () => {
+    result.textContent = i + 1;
+  });
+}
+submitBtn.addEventListener("click", () => {
+  rate.classList.add("hidden");
+  submit.classList.remove("hidden");
 });
